7542d96401e0bcaaefcf16bdb0c0ce6d102c19f6
Shorya Taneja <shoryataneja5@gmail.com>
2026-09-03 10:31:01 +0530

    useEff bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import RootLayout from '@layout/RootLayout'
 import Hero from '@sections/Hero'
 import Products from '@sections/Products'
